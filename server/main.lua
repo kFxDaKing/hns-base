fa5b834a87b8094785e73e6f0a5e9cf3a1f08903
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2019-11-12 17:41:43 -0500

    added Server watermark

---
+++ server/main.lua
@@ -0,0 +1,17 @@
+-----------------------------------------
+--          Ascent Hide n' Seek
+--             Base Gamemode
+--                 Server
+-----------------------------------------
+
+AddEventHandler('es:firstSpawn', function(source, user)
+    
+end)
+
+AddEventHandler('es:playerLoaded', function(source, user)
+    
+end)
+
+AddEventHandler('es:playerDropped', function(user)
+    
+end)
