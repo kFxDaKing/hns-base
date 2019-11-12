acd8d7a00705292f45974b1db5b599b56234b49a
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2019-11-12 01:16:23 -0500

    created __resource configuration

---
+++ __resource.lua
@@ -0,0 +1,25 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description "Ascent Hide n' Seek Base Resource"
+
+ui_page
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'client/main.lua'
+}
+
+files {
+
+}
+
+dependencies {
+    'essentialmode'
+}
+
+exports {
+
+}
