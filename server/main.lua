fb7b762dc6b320ec7a592d755a2e35135213e259
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2019-11-12 18:24:41 -0500

    added comment to distinguish constants and globals

--- server/main.lua
+++ server/main.lua
@@ -7,6 +7,7 @@
 print('============ [ Ascent Gaming ] ============')
 print(" Starting Ascent Hide n' Seek")
 
+-- Constants and Globals
 GAME_MAX_TIME  = 600 -- Seconds : 10 Minutes
 gameTime       = 0      -- How much time has passed in the current game?
 gameInProgress = false  -- Is a game in progress? If false, a game is not = lobby.
@@ -16,7 +17,11 @@ AddEventHandler('es:firstSpawn', function(source, user)
 end)
 
 AddEventHandler('es:playerLoaded', function(source, user)
-    
+    if gameInProgress == true then
+
+    else
+
+    end
 end)
 
 AddEventHandler('es:playerDropped', function(user)
