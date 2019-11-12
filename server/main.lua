a6c2bccc0fdc1164513e774767d1ecd9670a62f8
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2019-11-12 18:22:23 -0500

    added global variables, game-time constant, seperate thread for game-timer

--- server/main.lua
+++ server/main.lua
@@ -4,6 +4,13 @@
 --                 Server
 -----------------------------------------
 
+print('============ [ Ascent Gaming ] ============')
+print(" Starting Ascent Hide n' Seek")
+
+GAME_MAX_TIME  = 600 -- Seconds : 10 Minutes
+gameTime       = 0      -- How much time has passed in the current game?
+gameInProgress = false  -- Is a game in progress? If false, a game is not = lobby.
+
 AddEventHandler('es:firstSpawn', function(source, user)
     
 end)
@@ -14,4 +21,21 @@ end)
 
 AddEventHandler('es:playerDropped', function(user)
     
+end)
+
+Citizen.CreateThread(function()
+    while true do
+
+        gameTime++
+
+        -- Game ends
+        if gameTime == GAME_MAX_TIME then
+
+
+
+        end
+
+        Citizen.Wait(1000) -- One Second
+
+    end
 end)
