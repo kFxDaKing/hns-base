42799138d6abef3ebe41cdf5d3cc5675eb83866d
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2019-11-14 02:58:35 -0500

    added handling logic for asc:doUpdateGame. Utalizes already defined asct:startGame and startLobby on state change

--- client/main.lua
+++ client/main.lua
@@ -19,5 +19,11 @@ end)
 
 RegisterNetEvent('asc:doUpdateGame')
 AddEventHandler('asc:doUpdateGame', function(gameTime, gameStatus) -- gameTime = number of seconds passed for current gameStatus; gameStatus = true game is in progress, false game is not (lobby)
+    if isInGame and not gameStatus then
+        TriggerEvent('asc:startLobby')  -- Client was in a game but has recieved a message from the server that the game is over.
+    else if not isInGame and gameStatus then
+        TriggerEvent('asc:startGame')   -- Client was not in a game but has recieved a message from the server that one has started.
+    end
 
+    isInGame = gameStatus  -- Update the client with the current status, reguardless of any state-changes
 end)
