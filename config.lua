540bacc72711899b33d8b61b6332ea756cb750e9
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2019-11-14 18:48:43 -0500

    populated configuration values for GAME and LOBBY times

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-Config = {}
+Config           = {}
 
-Config.GameTime  = 600      -- Seconds : 10 Minutes
-Config.LobbyTime = 30       -- Seconds : .5 Minutes
+Config.GameTime  = 600      -- Seconds : 10 Minutes (Amount of seconds a game will last for)
+Config.LobbyTime = 30       -- Seconds : .5 Minutes (Amount of seconds a lobby will last for)
