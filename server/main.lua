fe60faafb0d9cc7fb3ec652e69d52240710cc274
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2019-11-14 02:43:21 -0500

    added LOBBY_MAX_TIME and included lobby time in global tick

--- server/main.lua
+++ server/main.lua
@@ -9,6 +9,7 @@ print(" Starting Ascent Hide n' Seek")
 
 -- Constants and Globals
 GAME_MAX_TIME  = 600    -- Seconds : 10 Minutes
+LOBBY_MAX_TIME = 30     -- Seconds : .5 Minutes
 gameTime       = 0      -- How much time has passed in the current game?
 gameInProgress = false  -- Is a game in progress? If false, a game is not = lobby.
 
@@ -33,12 +34,13 @@ Citizen.CreateThread(function()
         gameTime++
 
         -- Game ends
-        if gameTime == GAME_MAX_TIME then
+        if gameTime == GAME_MAX_TIME and gameInProgress then
             gameInProgress = false
-        else
+        else if gameTime == LOBBY_MAX_TIME and not gameInProgress
             gameInProgress = true
         end
 
+        -- gameTime = number of seconds passed for current gameStatus; gameStatus = true game is in progress, false game is not (lobby)
         TriggerClientEvent('asc:doUpdateGame', -1, gameTime, gameInProgress)
 
         Citizen.Wait(1000) -- One Second
