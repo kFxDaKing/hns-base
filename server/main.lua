c34cfdf991b85e278f1fc3b0e2ce0748a05c5769
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2019-11-14 23:18:21 -0500

    added asc:startLobby, asc:startGame. Relocated Global updating to these event's handlers

--- server/main.lua
+++ server/main.lua
@@ -13,6 +13,8 @@ LOBBY_MAX_TIME = Config.LobbyTime   --
 gameTime       = 0                  -- How much time has passed in the current game?
 gameInProgress = false              -- Is a game in progress? If false, a game is not = lobby.
 
+-- Player Specific Events
+
 AddEventHandler('es:firstSpawn', function(source, user)
     
 end)
@@ -29,16 +31,30 @@ AddEventHandler('es:playerDropped', function(user)
     
 end)
 
+-- Game Management
+
+RegsiterNetEvent('asc:startLobby')
+AddEventHandler('asc:startLobby', function()
+    gameInProgress = false
+    gameTime = 0
+end)
+
+RegsiterNetEvent('asc:startGame')
+AddEventHandler('asc:startGame', function()
+    gameInProgress = true
+    gameTime = 0
+end)
+
 Citizen.CreateThread(function()
     while true do
         gameTime++
 
         if gameTime == GAME_MAX_TIME and gameInProgress then -- a game's time limit has been reached and is in progress
-            gameInProgress = false
-            gameTime = 0
+            TriggerEvent('asc:startLobby')
+
         else if gameTime == LOBBY_MAX_TIME and not gameInProgress then -- a lobby's time limit has been reached and a game is not in progress
-            gameInProgress = true
-            gameTime = 0
+            TriggerEvent('asc:startGame')
+            
         end
 
         -- gameTime = number of seconds passed for current gameStatus; gameStatus = true game is in progress, false game is not (lobby)
